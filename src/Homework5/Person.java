373c6ee4d16fc3c901145238b8313a29dc6726b1
agafonovilya <agafonov.i.m@gmail.com>
2020-03-28 22:57:54 +0300

    Homework 5 implemented

---
+++ src/Homework5/Person.java
@@ -0,0 +1,35 @@
+package Homework5;
+
+public class Person {
+
+    private String fio;
+    private String position;
+    private String email;
+    private long phoneNumber;
+    private int salary;
+    private int age;
+
+    public Person(String fio, String position, String email, long phoneNumber, int salary, int age){
+        this.fio = fio;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return  "FIO:'" + fio + '\'' +
+                ", Position:'" + position + '\'' +
+                ", Email:'" + email + '\'' +
+                ", PhoneNumber:" + phoneNumber +
+                ", Salary:" + salary +
+                ", Age:" + age;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+}
---
+++ src/Homework5/TestPerson.java
@@ -0,0 +1,20 @@
+package Homework5;
+
+public class TestPerson {
+    public static void main(String[] args) {
+        Person[] persArray = new Person[5];
+        persArray[0] = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", 89112223344L, 20000, 45);
+        persArray[1] = new Person("Petr Petrov", "Manager", "petrov@mailbox.com", 89235467889L, 30000, 30);
+        persArray[2] = new Person("Kirill Kirillov", "Director", "kirillov@mailbox.com", 89994567890L, 50000, 55);
+        persArray[3] = new Person("Vasiliy Vasiliyev", "Actor", "vasiliy@mailbox.com", 898352345476L, 22500, 35);
+        persArray[4] = new Person("Nikolai Nikolaev", "Driver", "nikolaev@mailbox.com", 89057651234L, 31000, 51);
+
+        System.out.println("Выводим информацию о сотрудниках старше 40 лет:");
+        for (Person person: persArray) {
+            if(person.getAge() >= 40) {
+                System.out.println(person);
+            }
+        }
+
+    }
+}
